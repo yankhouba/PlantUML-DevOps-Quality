7f5ddbce1d6e90e7265801d11f0836fcf8e1ead2
yankhouba <111141798+yankhouba@users.noreply.github.com>
2026-03-11 15:09:20 -0400

    Ajout du premier test unitaire avec JUnit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testApplication() {
+        assertTrue(true);
+    }
+
+}
